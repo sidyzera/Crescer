38fdbbb7a3a73bc0caf0a685e4b6bdaa798a1af3
Sidinei Da Silva <sidybbc@gmail.com>
2021-02-19 19:49:18 -0300

    Cuidados com os numeros

---
+++ fundamentos/numerosAlgunsCuidados.js
@@ -0,0 +1,6 @@
+console.log(7 / 0) // Retorna Infinity
+console.log("10" / 2) // O JS consegue identificar e resolver a divisão
+console.log("Show!" * 2) // Esta função gera um "Not a numer"
+console.log(0.1 + 0.7) // Imprecisão 
+// console.log(10.toString()) Isso não é permitido.
+console.log((10.345).toFixed(2)) // Mas desta maneira é permitido
