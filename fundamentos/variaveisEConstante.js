acabb4f85bb08569d93be28a4a58373414e4dea1
Sidinei Da Silva <sidybbc@gmail.com>
2021-02-19 17:18:56 -0300

    Palavras reservadas.

---
+++ fundamentos/variaveisEConstante.js
@@ -0,0 +1,20 @@
+// Palavras reservadas da linguagem js.
+// Na opção "var" pode usar a mesma variavél varias vezes que não ira apresentar erros de variaveis.
+// Na opção "let" as variaveis poderam ser usadas uma unica vez.
+
+var a = 3
+let b = 4
+
+var a = 30
+b = 40
+
+console.log(a, b)
+
+a = 300
+b = 400
+
+console.log(a, b)
+
+const c = 5
+// c= 50 não é possiv4el em uma constante, para isso usamos a constante existente.
+console.log(c)
