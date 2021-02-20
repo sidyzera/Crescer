af9a85ef9694d97266a8ad42ff45ce33ad96d8e3
Sidinei Da Silva <sidybbc@gmail.com>
2021-02-19 22:56:04 -0300

    Tipo Objeto

---
+++ fundamentos/objeto.js
@@ -0,0 +1,13 @@
+const prod1 = {}
+prod1.nome = 'Celular Sanssung A71'
+prod1.preco = 2300.00
+prod1['Desconto'] = 0.40 //evitar atributos com espaços
+
+console.log(prod1)
+
+const prod2 = {
+    nome: 'Camisa Polo',
+    preco: 79.90
+}
+
+console.log(prod2)
