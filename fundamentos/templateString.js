cec14ea64b46749cacbc7f882ecb17a2885407fc
Sidinei Da Silva <sidybbc@gmail.com>
2021-02-19 22:18:28 -0300

    Template

---
+++ fundamentos/templateString.js
@@ -0,0 +1,13 @@
+const nome = 'Rebeca'
+const concatenacao = 'Olá ' + nome + '!'
+const templete = `
+    Olá
+    ${nome}!`
+console.log(concatenacao, templete)  
+
+// expressões...
+console.log(`1 + 1 = ${1 + 1}`)
+
+const up = texto => texto.toUpperCase()
+console.log(`Ei... ${up('cuidado')}!`)
+
