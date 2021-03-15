98b45b87bf528381b396ab7a496739a608e8d62e
sidyzera <sidybbc@gmail.com>
2021-03-15 00:03:14 -0300

    Exemplos de Arrays

--- Array/fulter2.js
+++ Array/fulter2.js
@@ -1,3 +1,13 @@
+Array.prototype.filter2 = function(callback){
+    const newArray = []
+    for(let i = 0; i < this.length; i++){
+        if(callback(this [i], i, this)){
+            newArray.push(this[i])
+        }
+    }
+    return newArray
+}
+
 const produtos = [
     { nome: 'Notebook', preco: 2499, fragil: true },
     { nome: 'iPad Pro', preco: 4199, fragil: true },
@@ -5,11 +15,7 @@ const produtos = [
     { nome: 'Copo de Plático', preco: 18.99, fragil: false }
 ]
 
-console.log(produtos.filter(function(p) {
-    return false
-}))
-
 const caro = produto => produto.preco >= 500
 const fragil = produto => produto.fragil
 
-console.log(produtos.filter(caro).filter(fragil))
+console.log(produtos.filter2(caro).filter2(fragil))
