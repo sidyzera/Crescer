7c2d0715b3b18cc94b74c32ae51dd0193b3188a9
sidyzera <sidybbc@gmail.com>
2021-04-23 16:53:18 -0300

    Teste

---
+++ Prova/Testes.js
@@ -0,0 +1,14 @@
+/*
+primeiro vc precisa transformar a string em array
+algo assim: let array = " tenho a melhor coisa do mundo ".split(" ");
+dai pra retornar a primeira letra pode ser algo assim:
+array.map(i => (r.substring(0, 1))
+em uma linha só: " tenho a melhor coisa do mundo ".split(" ").map(i => (i.substring(0, 1)))
+*/
+function solucao(arrA, arrB) {
+    let resposta = -1;
+
+    return resposta;
+}
+
+module.exports = { solucao }
